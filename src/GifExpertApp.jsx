71db1ab44be4496eb53776b3256d73eb325e3893
kocelly <kocelly@readymind.ms>
2022-04-15 23:31:09 -0500

    Creando una lista de cats

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories] = useState(['One Punch Man', 'Samurai X', 'Dragon Ball']);
+
+  const handleAdd = () => {
+    // setCategories( [...categories, 'Boku No Hero'] );
+    setCategories( categories => [ ...categories, 'Boku No Hero' ] );
+  }
+
   return (
     <>
       <h2>GifExpertApp</h2>
       <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {
+          categories.map( category => { 
+            return <li key={ category } > { category } </li>;
+          } )
+        }
+      </ol>
     </>
-  )
+  );
 }
