13c102dc30b33377008761c2501fb1f1d2440b17
kocelly <kocelly@readymind.ms>
2022-04-18 13:39:03 -0500

    helpers - getGifs

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
@@ -6,26 +7,10 @@ export const GifGrid = ({ category }) => {
   const [images, setImages] = useState([]);
 
   useEffect( () => {
-    getGifs();
-  }, []);
-
-  const getGifs = async() => {
-    const url = `https://api.giphy.com/v1/gifs/search?q=Spiderman&limit=10&api_key=QQEGXtfepQ6sc5nBg4wjQecq5DRwnzmC`;
-    const response = await fetch( url );
-    const { data } = await response.json();
-
-    const gifs = data.map( img => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url
-      }
-    })
-
-    console.log( gifs );
-
-    setImages( gifs );
-  }
+    getGifs( category )
+      // .then( imgs => setImages( imgs ) )
+      .then( setImages );
+  }, [ category ]);
 
   return (
     <>
